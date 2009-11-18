ca4c1ca2a8b41156a6828d139f66ffaf1af84cad
wagic.jeck <wagic.jeck@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2009-11-18 10:24:43 +0000

    Jeck - Removed WOTC trademark cheat profile.

--- projects/mtg/include/GameOptions.h
+++ projects/mtg/include/GameOptions.h
@@ -13,7 +13,7 @@ using std::string;
 #define PLAYER_SAVEFILE "data.dat"
 #define PLAYER_SETTINGS "options.txt"
 #define PLAYER_COLLECTION "collection.dat"
-#define SECRET_PROFILE "Gleemax"
+#define SECRET_PROFILE "Maxglee"
 
 #define INVALID_OPTION -1
 
