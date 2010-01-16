3e8c11b29fead2c36d0736d7068585a558277048
wagic.the.homebrew@gmail.com <wagic.the.homebrew@gmail.com@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2010-01-16 03:16:35 +0000

    Erwan -"fix" issue 257

--- projects/mtg/src/Token.cpp
+++ projects/mtg/src/Token.cpp
@@ -9,6 +9,7 @@ Token::Token(string _name, MTGCardInstance * source, int _power, int _toughness)
   lifeOrig = life;
   rarity = Constants::RARITY_T;
   name = _name;
+  if (name.size() && name[0]>=97 && name[0]<=122) name[0]-=32; //Poor man's camelcase. We assume strings we get are either Camelcased or lowercase
   setMTGId(- source->getMTGId());
   setId = source->setId;
   model = this;
