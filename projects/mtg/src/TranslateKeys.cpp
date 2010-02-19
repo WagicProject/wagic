4254d8cb9fb2cad692f4cb477e7528acb4cabea9
jean.chalard <jean.chalard@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2010-02-19 22:47:24 +0000

    J : * Put in text instead of numbers for the time being

--- projects/mtg/src/TranslateKeys.cpp
+++ projects/mtg/src/TranslateKeys.cpp
@@ -51,24 +51,24 @@ const KeyRep& translateKey(LocalKeySym key) {
   if ((res = fattable.find(key)) != fattable.end())
     return *(res->second);
 
-//  if (fattable.end() == fattable.find(PSP_CTRL_SELECT))
-//    {
-//      fattable[PSP_CTRL_SELECT]   = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)10*32, 0, 64, 32, "PSP_CTRL_SELECT", RETRIEVE_NORMAL), _("Select"));
-//      fattable[PSP_CTRL_START]    = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)8*32,  0, 64, 32, "PSP_CTRL_START", RETRIEVE_NORMAL), _("Start"));
-//      fattable[PSP_CTRL_UP]       = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)0*32,  0, 32, 32, "PSP_CTRL_UP", RETRIEVE_NORMAL), _("Up"));
-//      fattable[PSP_CTRL_RIGHT]    = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)3*32,  0, 32, 32, "PSP_CTRL_RIGHT", RETRIEVE_NORMAL), _("Right"));
-//      fattable[PSP_CTRL_DOWN]     = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)1*32,  0, 32, 32, "PSP_CTRL_DOWN", RETRIEVE_NORMAL), _("Down"));
-//      fattable[PSP_CTRL_LEFT]     = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)2*32,  0, 32, 32, "PSP_CTRL_LEFT", RETRIEVE_NORMAL), _("Left"));
-//      fattable[PSP_CTRL_LTRIGGER] = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)14*32, 0, 64, 32, "PSP_CTRL_LTRIGGER", RETRIEVE_NORMAL), _("Left trigger"));
-//      fattable[PSP_CTRL_RTRIGGER] = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)16*32, 0, 64, 32, "PSP_CTRL_RTRIGGER", RETRIEVE_NORMAL), _("Right trigger"));
-//      fattable[PSP_CTRL_TRIANGLE] = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)5*32,  0, 32, 32, "PSP_CTRL_TRIANGLE", RETRIEVE_NORMAL), _("Triangle"));
-//      fattable[PSP_CTRL_CIRCLE]   = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)4*32,  0, 32, 32, "PSP_CTRL_CIRCLE", RETRIEVE_NORMAL), _("Circle"));
-//      fattable[PSP_CTRL_CROSS]    = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)7*32,  0, 32, 32, "PSP_CTRL_CROSS", RETRIEVE_NORMAL), _("Cross"));
-//      fattable[PSP_CTRL_SQUARE]   = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)6*32,  0, 32, 32, "PSP_CTRL_SQUARE", RETRIEVE_NORMAL), _("Square"));
-//      fattable[PSP_CTRL_HOLD]     = new KeyRep(resources.RetrieveQuad("iconspsp.png", (float)4*32,  0, 32, 32, "PSP_CTRL_HOLD", RETRIEVE_NORMAL), _("Hold"));
-//    }
-//  if ((res = fattable.find(key)) != fattable.end())
-//    return *(res->second);
+  if (fattable.end() == fattable.find(PSP_CTRL_SELECT))
+    {
+      fattable[PSP_CTRL_SELECT]   = new KeyRep(NULL, _("Select"));
+      fattable[PSP_CTRL_START]    = new KeyRep(NULL, _("Start"));
+      fattable[PSP_CTRL_UP]       = new KeyRep(NULL, _("Up"));
+      fattable[PSP_CTRL_RIGHT]    = new KeyRep(NULL, _("Right"));
+      fattable[PSP_CTRL_DOWN]     = new KeyRep(NULL, _("Down"));
+      fattable[PSP_CTRL_LEFT]     = new KeyRep(NULL, _("Left"));
+      fattable[PSP_CTRL_LTRIGGER] = new KeyRep(NULL, _("Left trigger"));
+      fattable[PSP_CTRL_RTRIGGER] = new KeyRep(NULL, _("Right trigger"));
+      fattable[PSP_CTRL_TRIANGLE] = new KeyRep(NULL, _("Triangle"));
+      fattable[PSP_CTRL_CIRCLE]   = new KeyRep(NULL, _("Circle"));
+      fattable[PSP_CTRL_CROSS]    = new KeyRep(NULL, _("Cross"));
+      fattable[PSP_CTRL_SQUARE]   = new KeyRep(NULL, _("Square"));
+      fattable[PSP_CTRL_HOLD]     = new KeyRep(NULL, _("Hold"));
+    }
+  if ((res = fattable.find(key)) != fattable.end())
+    return *(res->second);
   char* str = new char[11];
   sprintf(str, "%d", key);
   KeyRep* k = new KeyRep(NULL, str);
