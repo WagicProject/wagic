817a666a2f1699047b211524fd193c9d5cad74ee
Xawotihs <Xawotihs@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2010-10-16 12:13:07 +0000

    Fixed Qt compilation on Windows

--- projects/mtg/include/DebugRoutines.h
+++ projects/mtg/include/DebugRoutines.h
@@ -14,12 +14,21 @@
 #if defined (WIN32) || defined (LINUX)
 #ifdef _DEBUG
 
+#ifndef QT_CONFIG
 #define DebugTrace(inString)								\
 {																						\
 	std::ostringstream stream;								\
 	stream << inString << std::endl;					\
 	OutputDebugString(stream.str().c_str());	\
 }
+#else
+#define DebugTrace(inString)								\
+{																						\
+	std::ostringstream stream;								\
+	stream << inString << std::endl;					\
+	qDebug(stream.str().c_str());	            \
+}
+#endif //QT_CONFIG
 
 #endif //#ifdef _DEBUG
 #endif // Win32, Linux
