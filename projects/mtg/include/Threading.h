75345469ee443bf9375582290be7525f8c7f86ad
wrenczes@gmail.com <wrenczes@gmail.com@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2010-11-26 19:41:15 +0000

    File addition for my threading work.  This has no impact on the build until I tie in my other changes.

---
+++ projects/mtg/include/Threading.h
@@ -0,0 +1,51 @@
+#ifndef THREADING_H
+#define THREADING_H
+
+
+#if defined (WIN32) || defined (LINUX)
+
+#include <boost/date_time.hpp>
+#include <boost/thread.hpp>
+
+#else
+#include "pspthreadman.h"
+
+namespace boost
+{
+
+    class mutex
+    {
+    public:
+        struct scoped_lock
+        {
+            scoped_lock(mutex& inMutex) : mID(inMutex.mID)
+            {
+                sceKernelWaitSema(mID, 1, 0);
+            }
+
+            ~scoped_lock()
+            {
+                sceKernelSignalSema(mID, 1);
+            }
+
+            int mID;
+        };
+
+        mutex()
+        {
+            mID = sceKernelCreateSema("Unnamed", 0, 1, 1, 0);
+        }
+
+        ~mutex()
+        {
+            sceKernelDeleteSema(mID);
+        }
+
+        int mID;
+    };
+    
+}
+
+#endif
+
+#endif // THREADING_H
