02b04e65244616ff190bfca95a63dba0dc342cfb
Xawotihs <Xawotihs@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2011-12-29 21:28:45 +0000

    Adds a python script to create the resource zip package

---
+++ projects/mtg/bin/Res/createResourceZip.py
@@ -0,0 +1,44 @@
+import zipfile
+import os
+
+
+class ZipUtilities:
+
+    def toZip(self, file, filename):
+        zip_file = zipfile.ZipFile(filename, 'w')
+        if os.path.isfile(file):
+                        zip_file.write(file)
+        else:
+                        self.addFolderToZip(zip_file, file)
+        zip_file.close()
+
+    def addFolderToZip(self, zip_file, folder): 
+        zip_file.writestr(folder + '/', '')
+        for file in os.listdir(folder):
+            if file != '.svn':
+                full_path = os.path.join(folder, file)
+                if os.path.isfile(full_path):
+                        print 'File added: ' + str(full_path)
+                        zip_file.write(full_path)
+                elif os.path.isdir(full_path):
+                        print 'Entering folder: ' + str(full_path)
+                        self.addFolderToZip(zip_file, full_path)
+
+def main():
+    utilities = ZipUtilities()
+    filename = 'Res.zip'
+    zip_file = zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
+    utilities.addFolderToZip(zip_file, 'themes')
+    utilities.addFolderToZip(zip_file, 'sound')
+    utilities.addFolderToZip(zip_file, 'settings')
+    utilities.addFolderToZip(zip_file, 'sets')
+    utilities.addFolderToZip(zip_file, 'rules')
+    utilities.addFolderToZip(zip_file, 'player')
+    utilities.addFolderToZip(zip_file, 'packs')
+    utilities.addFolderToZip(zip_file, 'lang')
+    utilities.addFolderToZip(zip_file, 'graphics')
+    utilities.addFolderToZip(zip_file, 'campaigns')
+    utilities.addFolderToZip(zip_file, 'ai')
+    zip_file.close()
+
+main()
