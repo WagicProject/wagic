f80127dff3e35610d715ba6ea889a1af0680ef85
techdragon.nguyen@gmail.com <techdragon.nguyen@gmail.com@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2012-04-17 11:24:02 +0000

    modified Android build to allow between a DEBUG vs RELEASE build of the native code.  When doing an ndk build run the following to do a release build 'DEBUG=RELEASE ndk-build'

--- projects/mtg/Android/jni/Application.mk
+++ projects/mtg/Android/jni/Application.mk
@@ -3,3 +3,6 @@ APP_CPPFLAGS += -frtti -fexceptions
 APP_ABI := armeabi armeabi-v7a
 APP_STL := gnustl_static
 APP_MODULES := libpng libjpeg main SDL
+
+#APP_OPTIM is 'release' by default
+APP_OPTIM := release
