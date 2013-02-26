ef18f376b13c85010ff2fcf7885be07f0756b0d1
omegablast2002@yahoo.com <omegablast2002@yahoo.com@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2013-02-26 23:22:49 +0000

    ifdef(psp) on cache hit miss handling.

--- projects/mtg/src/WResourceManager.cpp
+++ projects/mtg/src/WResourceManager.cpp
@@ -1167,10 +1167,11 @@ cacheItem* WCache<cacheItem, cacheActual>::Get(int id, const string& filename, i
 
     // check if we're doing a card lookup
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /*  commented out for the following reason:
-    cardgui updates so quick that the only reason a person would notice a cache miss is because it takes the 
-    engine longer to switch the card quad from kGeneric to the actual image, than it does to just let the update pass.
-  if (submode & TEXTURE_SUB_CARD)
+    //  commented out for the following reason:
+    //cardgui updates so quick that the only reason a person would notice a cache miss is because it takes the 
+    //engine longer to switch the card quad from kGeneric to the actual image, than it does to just let the update pass.
+#if defined (PSP) 
+    if (submode & TEXTURE_SUB_CARD)
     {
         // processing a cache miss, return a generic card & queue up an async read
 
@@ -1183,7 +1184,8 @@ cacheItem* WCache<cacheItem, cacheActual>::Get(int id, const string& filename, i
         CacheEngine::Instance()->QueueRequest(filename, submode, lookup);
         return it->second;
     } 
-    */
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Space in cache, make new texture
     return LoadIntoCache(lookup, filename, submode, style);
