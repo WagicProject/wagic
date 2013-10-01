da7c336dc233ae59ce7bf6640da0b3b4427d7921
pankdm <pankdm@01922e74-a4c3-11dd-a5cd-697bfd0a0cef>
2013-10-01 14:49:31 +0000

    Removed debug code that was added by mistake

--- projects/mtg/src/TestSuiteAI.cpp
+++ projects/mtg/src/TestSuiteAI.cpp
@@ -552,10 +552,6 @@ int TestSuite::loadNext()
         return loadNext();
     }
 
-    mProcessing = true;
-    ThreadProc(this);
-    return 0;
-
     if(!mProcessing)
     {   // "I don't like to wait" mode
         mProcessing = true;
