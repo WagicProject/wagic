bf0a0a1580eba5233e5594329323cdaf366f8b79
xawotihs <xawotihs@gmail.com>
2013-11-30 19:32:55 +0100

    Build the graphics version in release instead of debug.

--- travis-script.sh
+++ travis-script.sh
@@ -18,11 +18,11 @@ ant debug -f projects/mtg/Android/build.xml
 # we're building a Qt version with GUI here
 mkdir qt-gui-build
 cd qt-gui-build
-qmake ../projects/mtg/wagic-qt.pro CONFIG+=debug CONFIG+=graphics
+qmake ../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
 make -j 8
 cd ..
 
-# let's try an Intel linux binary
+# let's try an Intel linux binary in debug text-mode-only
 qmake projects/mtg/wagic-qt.pro CONFIG+=console CONFIG+=debug DEFINES+=CAPTURE_STDERR
 make -j 8
 
