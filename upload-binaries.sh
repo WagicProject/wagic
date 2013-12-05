826403a5122904435eaf202ddb7a20b98835cfc6
xawotihs <xawotihs@gmail.com>
2013-12-05 22:44:02 +0100

    Fixed upload shell script

--- upload-binaries.sh
+++ upload-binaries.sh
@@ -1,6 +1,5 @@
-#!/bin/sh -ex
-
-if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "ci_upload_binaries"]; then
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+if [ "$TRAVIS_BRANCH" == "ci_upload_binaries" ]; then
   echo -e "Creating a release\n"
   curl -X POST -H "Authorization: token ${GH_TOKEN}" \
       -d '{"tag_name": "pre-release-${TRAVIS_BUILD_NUMBER}", "target_commitish": "master", "name": "pre-release-${TRAVIS_BUILD_NUMBER}", "body": "Automatic pre-release ${TRAVIS_BUILD_NUMBER}", "draft": true, "prerelease": true}' "https://api.github.com/repos/WagicProject/wagic/releases"
@@ -16,4 +15,5 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "ci_upload_binarie
      "https://uploads.github.com/repos/WagicProject/wagic/releases/pre-release-${TRAVIS_BUILD_NUMBER}/assets?name=Wagic-android-${TRAVIS_BUILD_NUMBER}.apk"
 
   echo -e "Done uploading\n"
+fi
 fi
