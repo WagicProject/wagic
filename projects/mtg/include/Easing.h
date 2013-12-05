77eb7f2777a3aa74d87e02204aefa90df3a9958a
Tobias Loose <TobiasLoose@gmx.de>
2013-12-05 22:54:44 +0100

    Add missing header

---
+++ projects/mtg/include/Easing.h
@@ -0,0 +1,72 @@
+#ifndef _EASING_H_
+#define _EASING_H_
+
+//see http://www.gizma.com/easing/ for more information
+class Easing
+{
+public:
+    float start_value;
+    float delta_value;
+    float value;
+    float duration;
+    float time_acc;
+
+
+    Easing(float val): start_value(val), delta_value(0), value(val), duration(0), time_acc(0)
+    {}
+
+    void reset(){ value = start_value; time_acc = 0;}
+    void finish(){ value = start_value + delta_value; time_acc = 0; duration = 0;}
+    virtual void update(float dt) = 0;
+
+    void start(float targetValue, float _duration){
+        start_value = value;
+        delta_value = targetValue - start_value;
+        time_acc = 0;
+        duration = _duration;
+    }
+
+    void translate(float delta_value){
+        start_value += delta_value;
+        value += delta_value;
+    }
+
+    bool finished()
+    {
+        return time_acc >= duration;
+    }
+};
+
+class InOutQuadEasing : public Easing
+{
+public:
+    InOutQuadEasing(float val): Easing(val) {}
+
+    void update(float dt){
+        if(duration > 0){
+            time_acc += dt;
+
+            if(time_acc > duration)
+            {
+                time_acc = duration;
+                value = start_value + delta_value;
+            }
+            else
+            {
+                float time_tmp = time_acc * 2 / duration;
+                if (time_tmp < 1)
+                {
+                    value = delta_value/2*time_tmp*time_tmp + start_value;
+                }
+                else
+                {
+                    time_tmp -= 1;
+                    value = -delta_value/2 * (time_tmp*(time_tmp-2) - 1) + start_value;
+                }
+            }
+        }
+    }
+};
+
+
+#endif //_EASING_H_
