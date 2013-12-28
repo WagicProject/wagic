fe9ad84ee50cbf2debd143944b90b44a1ee2d98a
xawotihs <xawotihs@gmail.com>
2013-12-28 12:15:52 +0100

    More PSP compilation fixes.

--- JGE/src/JAudio.cpp
+++ JGE/src/JAudio.cpp
@@ -49,7 +49,8 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 {
 
 	JFileSystem* fileSystem = JFileSystem::GetInstance();
-	if (!fileSystem->OpenFile(fileName))
+    JFile* jFile = fileSystem->OpenFile(fileName);
+    if (!jFile)
 		return 0;
 
 	memset(p_wav, 0, sizeof(WAVDATA));
@@ -57,14 +58,14 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 	char head[256];
 	memset(head, 0, 256);
 	//sceIoRead(fd, head, 20);
-	fileSystem->ReadFile(head, 20);
+    fileSystem->ReadFile(jFile, head, 20);
 	char string[8];
 	memset(string, 0, 8);
 	memcpy(string, head, 4);
 	if (0!=strcmp(string, "RIFF"))
 	{
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 		return 0;
 	}
 	memset(string, 0, 8);
@@ -72,7 +73,7 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 	if (0!=strcmp(string, "WAVE"))
 	{
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 		return 0;
 	}
 	memset(string, 0, 8);
@@ -80,42 +81,42 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 	if (0!=strcmp(string, "fmt"))
 	{
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 		return 0;
 	}
 	int fmtSize = 0;
 	memcpy(&fmtSize, head+16, 4);
 	//sceIoRead(fd, head+20, fmtSize);
-	fileSystem->ReadFile(head+20,fmtSize );
+    fileSystem->ReadFile(jFile, head+20,fmtSize );
 	p_wav->headSize = 20+fmtSize;
 	while (1)
 	{
 		//sceIoRead(fd, head+p_wav->headSize, 4);
-		fileSystem->ReadFile(head+p_wav->headSize, 4);
+        fileSystem->ReadFile(jFile, head+p_wav->headSize, 4);
 		memset(string, 0, 8);
 		memcpy(string, head+p_wav->headSize, 4);
 		p_wav->headSize += 4;
 		if (0!=strcmp(string, "data"))
 		{
 			//sceIoRead(fd, head+p_wav->headSize, 4);
-			fileSystem->ReadFile(head+p_wav->headSize, 4);
+            fileSystem->ReadFile(jFile, head+p_wav->headSize, 4);
 			memcpy(&fmtSize, head+p_wav->headSize, 4);
 			p_wav->headSize += 4;
 			//sceIoRead(fd, head+p_wav->headSize, fmtSize);
-			fileSystem->ReadFile(head+p_wav->headSize, fmtSize);
+            fileSystem->ReadFile(jFile, head+p_wav->headSize, fmtSize);
 			p_wav->headSize += fmtSize;
 		}
 		else
 		{
 			//sceIoRead(fd, head+p_wav->headSize, 4);
-			fileSystem->ReadFile(head+p_wav->headSize, 4);
+            fileSystem->ReadFile(jFile, head+p_wav->headSize, 4);
 			p_wav->headSize += 4;
 			break;
 		}
 		if (p_wav->headSize>191)
 		{
 			//sceIoClose(fd);
-			fileSystem->CloseFile();
+            fileSystem->CloseFile(jFile);
 			return 0;
 		}
 	}
@@ -126,7 +127,7 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 	if (p_wav->channelCount!=1 && p_wav->channelCount!=2)
 	{
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 		return 0;
 	}
 	memcpy(&p_wav->samplePerSecond, head+24, 4);
@@ -136,7 +137,7 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 	if (p_wav->bytePerSample!=1 && p_wav->bytePerSample!=2)
 	{
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 		return 0;
 	}
 	p_wav->nSample = 44100 / p_wav->samplePerSecond;
@@ -147,17 +148,17 @@ char loadWaveData(WAVDATA* p_wav, char* fileName, char memLoad)  // WAVE加载, 
 		if (p_wav->soundSize>4096000)
 		{
 			//sceIoClose(fd);
-			fileSystem->CloseFile();
+            fileSystem->CloseFile(jFile);
 			return 0;
 		}
 		p_wav->buffer = (char*)malloc(p_wav->soundSize);
 		memset(p_wav->buffer, 0, p_wav->soundSize);
 		//sceIoRead(fd, p_wav->buffer, p_wav->soundSize);
-		fileSystem->ReadFile(p_wav->buffer, p_wav->soundSize);
+        fileSystem->ReadFile(jFile, p_wav->buffer, p_wav->soundSize);
 		p_wav->bytePosition = 0;
 		p_wav->fd = -1;
 		//sceIoClose(fd);
-		fileSystem->CloseFile();
+        fileSystem->CloseFile(jFile);
 	}
 	else
 	{
