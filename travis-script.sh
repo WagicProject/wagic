2d61ff6e2e84a2e6293e7246d667433af4eba22c
xawotihs <xawotihs@gmail.com>
2014-08-05 23:22:45 +0200

    Trying matrix build

--- travis-script.sh
+++ travis-script.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -ex
 
 # let's dump some info to debug a bit
-echo PSPDEV = $PSPDEV
-echo psp-config = `psp-config --psp-prefix`
 echo ls = `ls`
 echo pwd = `pwd`
 # computing potential release name
@@ -35,66 +33,74 @@ mv core_*.zip ../../../../core.zip
 cd ../../../..
 
 # we're building a PSP binary here
-cd JGE
-make -j 8
-cd ..
-cd projects/mtg
-mkdir objs
-make -j 8
-mkdir WTH
-mkdir WTH/Res
-mv EBOOT.PBP WTH/
-mv ../../JGE/exceptionHandler/prx/exception.prx WTH/
-cp ../../core.zip WTH/Res
-cd WTH/Res
-unzip core.zip
-rm core.zip
-cd ..
-chmod -R 775 Res
-cd ..
-zip psprelease.zip -r WTH/
-cd ../..
+if [ "$BUILD_TYPE" == "PSP"]; then
+ # let's dump some info to debug a bit
+ echo PSPDEV = $PSPDEV
+  echo psp-config = `psp-config --psp-prefix`
+  cd JGE
+  make -j 8
+  cd ..
+  cd projects/mtg
+  mkdir objs
+  make -j 8
+  mkdir WTH
+  mkdir WTH/Res
+  mv EBOOT.PBP WTH/
+  mv ../../JGE/exceptionHandler/prx/exception.prx WTH/
+  cp ../../core.zip WTH/Res
+  cd WTH/Res
+  unzip core.zip
+  rm core.zip
+  cd ..
+  chmod -R 775 Res
+  cd ..
+  zip psprelease.zip -r WTH/
+  cd ../..
+fi
 
 # we're building an Android binary here
-android-ndk-r9/ndk-build -C projects/mtg/Android -j8
-$ANDROID list targets
-$ANDROID update project -t 1 -p projects/mtg/Android
-ant debug -f projects/mtg/Android/build.xml
+if [ "$BUILD_TYPE" == "Android"]; then
+  android-ndk-r9/ndk-build -C projects/mtg/Android -j8
+  $ANDROID list targets
+  $ANDROID update project -t 1 -p projects/mtg/Android
+  ant debug -f projects/mtg/Android/build.xml
+fi
 
-# we're building a Qt version with GUI here
-mkdir qt-gui-build
-cd qt-gui-build
-$QMAKE ../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
-make -j 8
-cd ..
+if [ "$BUILD_TYPE" == "Qt"]; then
+  # we're building a Qt version with GUI here
+  mkdir qt-gui-build
+  cd qt-gui-build
+  $QMAKE ../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
+  make -j 8
+  cd ..
+  # let's try an Intel linux binary in debug text-mode-only
+  $QMAKE projects/mtg/wagic-qt.pro CONFIG+=console CONFIG+=debug DEFINES+=CAPTURE_STDERR
+  make -j 8
 
-# let's try an Intel linux binary in debug text-mode-only
-$QMAKE projects/mtg/wagic-qt.pro CONFIG+=console CONFIG+=debug DEFINES+=CAPTURE_STDERR
-make -j 8
+  # we're cross-compiling a Qt Windows version here,
+  # PATH is only set here to prevent colision
 
-# we're cross-compiling a Qt Windows version here, 
-# PATH is only set here to prevent colision
+  # export PATH="$PATH:/opt/mingw32/bin"
+  # mkdir build
+  # cd build
+  # mkdir win-cross
+  # cd win-cross
+  # /opt/mingw32/bin/qmake ../../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
+  # make -j 8
+  # cd release
+  # cp ../../../projects/mtg/bin/fmod.dll .
+  # cp /opt/mingw32/bin/QtCore4.dll .
+  # cp /opt/mingw32/bin/QtGui4.dll .
+  # cp /opt/mingw32/bin/QtNetwork4.dll .
+  # cp /opt/mingw32/bin/QtOpenGL4.dll .
+  # cp ../../../projects/mtg/bin/zlib1.dll .
+  # cp /opt/mingw32/bin/libpng15-15.dll .
+  # cd ..
+  # zip win-cross.zip -r release/
+  # cd ../..
 
-# export PATH="$PATH:/opt/mingw32/bin"
-# mkdir build
-# cd build
-# mkdir win-cross
-# cd win-cross
-# /opt/mingw32/bin/qmake ../../projects/mtg/wagic-qt.pro CONFIG+=release CONFIG+=graphics
-# make -j 8
-# cd release
-# cp ../../../projects/mtg/bin/fmod.dll .
-# cp /opt/mingw32/bin/QtCore4.dll .
-# cp /opt/mingw32/bin/QtGui4.dll .
-# cp /opt/mingw32/bin/QtNetwork4.dll .
-# cp /opt/mingw32/bin/QtOpenGL4.dll .
-# cp ../../../projects/mtg/bin/zlib1.dll .
-# cp /opt/mingw32/bin/libpng15-15.dll .
-# cd ..
-# zip win-cross.zip -r release/
-# cd ../..
-
-# Now we run the testsuite (Res needs to be in the working directory)
-cd projects/mtg
-../../wagic
-cd ../..
+  # Now we run the testsuite (Res needs to be in the working directory)
+  cd projects/mtg
+  ../../wagic
+  cd ../..
+fi
