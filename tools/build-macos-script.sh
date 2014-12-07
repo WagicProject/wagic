c451234bd5b858eb7a93ca85e20c647dab5368fc
xawotihs <xawotihs@gmail.com>
2014-12-07 22:47:04 +0100

    Moved definition of origin

--- tools/build-macos-script.sh
+++ tools/build-macos-script.sh
@@ -11,6 +11,7 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git config --global user.name $GH_USER
 git config --global user.email $GH_EMAIL
+git remote set-url origin "https://${GH_TOKEN}@github.com/WagicProject/wagic.git"
 
 ## Delete remote Travis-Mac branch (if any)
 export REMOTE=$(git branch -r | grep "origin/$TRAVIS_MAC_BRANCH\$")
@@ -29,6 +30,5 @@ cp tools/macos.travis.yml .travis.yml
 git add .travis.yml
 git rm appveyor.yml
 git commit -m "Auto-Updated Travis-CI configuration for Mac"
-git remote set-url origin "https://${GH_TOKEN}@github.com/WagicProject/wagic.git"
 ## Push new branch to remote server
 git push -q origin $TRAVIS_MAC_BRANCH:$TRAVIS_MAC_BRANCH
