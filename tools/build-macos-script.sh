dbe1c6e2aee08d0f5f75a84aabd5fe4cbe7b3cf5
xawotihs <xawotihs@gmail.com>
2014-12-07 17:26:32 +0100

    Moved mac travis file outside of script and update oauth token

--- tools/build-macos-script.sh
+++ tools/build-macos-script.sh
@@ -3,21 +3,6 @@
 ## New of branch to use
 TRAVIS_MAC_BRANCH=travis_mac_osx
 
-## New Travis-CI configuration, adapted for Mac-OS
-NEW_TRAVIS_YML=\
-"language: objective-c
-before_install:
-- brew update
-- brew install qt5
-env:
-  global:
-    secure: "fJgWlCFbde96OSQNGKUmowGX+ERPeqP+n1EOMf1+FJzOU4DdkTLRAlV5+5qnEX9jB/3mWN6iPpmG1qEz/SdDG3KHxJYs4ZU/Lu485O24zZ/+GdYBNsrvhPD9ckPGEMLDa1foEVTDnW0Dlkz3BCFcszjhtXGUJv7v6Pj6LRk1Mg8="
-script:
-- /usr/local/opt/qt5/bin/qmake projects/mtg/wagic-qt.pro CONFIG+=graphics
-- make -j 4 dmg
-after_success:
-- python tools/upload-binaries.py -t $GH_TOKEN -s $TRAVIS_COMMIT -l wagic.dmg -r Wagic-macosx.dmg -b $TRAVIS_BRANCH"
-
 ## Only cross-compile on Mac the master branch
 test "$TRAVIS_BRANCH" != "master" && exit 0
 
@@ -38,7 +23,7 @@ fi
 git checkout -q -b "$TRAVIS_MAC_BRANCH" "$TRAVIS_BRANCH"
 
 ## Write a new Travis-CI configuration file
-echo "$NEW_TRAVIS_YML" > .travis.yml
+cp tools/mac.travis.yml .travis.yml
 git add .travis.yml
 git rm appveyor.yml
 git commit -m "Auto-Updated Travis-CI configuration for Mac"
