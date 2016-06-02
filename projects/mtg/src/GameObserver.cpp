956d0a736d746b6c00febf85fcc21983607b873c
xawotihs <xawotihs@gmail.com>
2016-06-02 20:57:09 +0200

    Should fix Macosx build

--- projects/mtg/src/GameObserver.cpp
+++ projects/mtg/src/GameObserver.cpp
@@ -1666,7 +1666,7 @@ ostream& operator<<(ostream& out, const GameObserver& g)
         out << g.mSeed;
         out << endl;
         out << "rvalues:";
-        out << g.randomGenerator.saveUsedRandValues(out);
+        g.randomGenerator.saveUsedRandValues(out);
         out << endl;
         out << g.startupGameSerialized;
     }
