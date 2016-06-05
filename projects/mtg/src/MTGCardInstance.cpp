7ae8434ba370e59fd10eeea2a3f0dedf0ceb1c1c
Anthony Calosa <anthonycalosa@gmail.com>
2016-06-05 20:32:33 +0800

    Correction

--- projects/mtg/src/MTGCardInstance.cpp
+++ projects/mtg/src/MTGCardInstance.cpp
@@ -118,9 +118,14 @@ void MTGCardInstance::copy(MTGCardInstance * card)
     AbilityFactory af(observer);
     af.addAbilities(observer->mLayers->actionLayer()->getMaxId(), spell);
     delete spell;
-    mtgid = card->getMTGId();   ///////////////////////////////////////////////////
-    setId = card->setId;        // Copier/Cloner cards produces the same token...//
-    rarity = card->getRarity(); ///////////////////////////////////////////////////
+    if(observer->players[1]->playMode == Player::MODE_TEST_SUITE)
+        mtgid = backupid; // there must be a way to get the token id...
+    else
+    {
+        mtgid = card->getMTGId();   ///////////////////////////////////////////////////
+        setId = card->setId;        // Copier/Cloner cards produces the same token...//
+        rarity = card->getRarity(); ///////////////////////////////////////////////////
+    }
     castMethod = castMethodBackUP;
     backupTargets = this->backupTargets;
     storedCard = oldStored;
