1d2abeb20be69f16ff2ec5dd77b4369e7ed63933
Anthony Calosa <anthonycalosa@gmail.com>
2016-07-29 18:37:03 +0800

    Minor Border changes
    Also added copier values if it has copied a token or not...

--- projects/mtg/src/CardDisplay.cpp
+++ projects/mtg/src/CardDisplay.cpp
@@ -263,11 +263,11 @@ void CardDisplay::Render()
     {
         mObjects[mCurr]->Render();
         CardGui * cardg = ((CardGui *) mObjects[mCurr]);
-        Pos pos = Pos(CardGui::BigWidth / 2, CardGui::BigHeight / 2 - 10, 1.0, 0.0, 220);
-        int drawMode = DrawMode::kNormal;
+        Pos pos = Pos(CardGui::BigWidth / 2, CardGui::BigHeight / 2 - 10, 0.90f, 0.0, 220);
+        int drawMode = DrawMode::kNormal;//scale card display from 1.0f to 0.90f
         if (observer)
         {
-            pos.actY = 145;
+            pos.actY = 142;//reduce y a little
             if (x < (CardGui::BigWidth / 2)) pos.actX = SCREEN_WIDTH - 10 - CardGui::BigWidth / 2;
             drawMode = observer->getCardSelector()->GetDrawMode();
         }
