84fe9fbc74e882f4ff215ef0a2cdb9dfb6954d62
Anthony Calosa <anthonycalosa@gmail.com>
2017-03-05 22:38:53 +0800

    highlight border fix
    all selected/targeted cards ar bordered red while the source is green
    border

--- projects/mtg/src/ActionStack.cpp
+++ projects/mtg/src/ActionStack.cpp
@@ -241,6 +241,7 @@ void StackAbility::Render()
     MTGCardInstance * source = ability->source;
     string alt1 = source->getName();
     vector<JQuadPtr> mytargetQuads;
+    vector<MTGCardInstance*> myClones;
 
     int fmLibrary = 0;
     int force = 0;
@@ -250,10 +251,7 @@ void StackAbility::Render()
     {
         Targetable * t = ability->getActionTc()->getNextTarget();
         if (t)
-        {
-            source->forcedBorderA = 1;
             _target = t;
-        }
 
         
     //test vector quads
@@ -266,7 +264,8 @@ void StackAbility::Render()
                 {
                     if( ((Damageable *)(tt))->type_as_damageable == Damageable::DAMAGEABLE_MTGCARDINSTANCE )
                     {
-                        ((MTGCardInstance*)(tt))->forcedBorderB = 1;
+                        //fill vector
+                        myClones.push_back(((MTGCardInstance*)(tt)));
 
                         if( source->has(Constants::HIDDENFACE) && !observer->isInLibrary(((MTGCardInstance *)(tt))) )
                             mytargetQuads.push_back( ((Damageable *)(tt))->getIcon() );
@@ -296,6 +295,20 @@ void StackAbility::Render()
         if (target->type_as_damageable == Damageable::DAMAGEABLE_MTGCARDINSTANCE)
         {
             alt2 = ((MTGCardInstance *) target)->name;
+        }
+    }
+
+    //setborder test
+    if(myClones.size())
+    {
+        source->forcedBorderB = 1;
+        for(unsigned int kk = 0; kk < myClones.size(); kk++)
+        {
+            if(myClones[kk])
+            {
+                myClones[kk]->forcedBorderA = 1;
+                //JRenderer::GetInstance()->DrawLine(myClones[kk]->view->actX,myClones[kk]->view->actY,source->view->actX,source->view->actY,0.5f,ARGB(120, 255, 0, 0));
+            }
         }
     }
 
