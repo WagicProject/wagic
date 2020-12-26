98f749983e944d4baf7fce4f73f2e3db9c78cf01
Vittorio Alfieri <vittorioalfieri@gmail.com>
2020-12-26 11:27:29 +0100

    Test with newer SDK and NDK.

--- projects/mtg/Android/jni/Application.mk
+++ projects/mtg/Android/jni/Application.mk
@@ -2,7 +2,7 @@ APP_PROJECT_PATH := $(call my-dir)/..
 APP_CPPFLAGS += -frtti -fexceptions
 APP_ABI := armeabi armeabi-v7a
 #APP_ABI := x86 # mainly for emulators
-APP_STL := gnustl_static
+APP_STL := c++_static
 APP_MODULES := libpng libjpeg main SDL
 
 #APP_OPTIM is 'release' by default
